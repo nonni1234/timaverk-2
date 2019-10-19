782bb8efce88ddf0d31844aafb67d51c5b46e9bf
unknown <nonni1234567@gmail.com>
2019-10-19 15:50:56 +0000

    ég nenni þessu ekki

--- app.py
+++ app.py
@@ -3,26 +3,31 @@ from flask import Flask, render_template as template, session, url_for, request,
 app = Flask(__name__)
 app.secret_key = os.urandom(8)
 # Gera stuff tilbúið
-
 teljari = 0
 with open("static/quiz.json", "r", encoding="UTF-8") as f:
     quizzes= json.load(f)
     quizzes = quizzes["quizzes"]
-
+# -----------------
 
 @app.route("/") # Root Routeið
 def home():
+    return template("index.html")
+
+@app.route("/quiz/<name>")
+def quizzes(name):
     teljari = 0 # Redefinear teljara
     if "teljari" in session: # Ef teljari er í session þá er hann notaður
         teljari = session["teljari"]
     else: # Ef ekki þá er hann settur inn í session
         session["teljari"] = teljari
-    return template("index.html",spurning = quizzes["shrek"][teljari])
+
+    return template("quiz.html",spurning = quizzes[name][teljari])
+
 
 @app.route("/next") # Næsta spurning (hækkar teljara um eitt)
-def addTel(teljari=teljari):
-    if "teljari" in session: # Notar teljara inn í session ef hann er til
-        teljari = session["teljari"]
+def next():
+    if "teljarar" in session: # Notar teljara inn í session ef hann er til
+        
         print(teljari)
         if teljari >= len(quizzes["shrek"])-1:
             teljari = 0
@@ -32,7 +37,7 @@ def addTel(teljari=teljari):
         
     else: # Ef það er enginn teljari inn í Session þá gerist ekkert
         pass
-    return redirect(url_for("home"))
+    return redirect(url_for("quizzes"))
 
     
 
