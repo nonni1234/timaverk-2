775a7401c880f4f689d9247513d9c3e9c247b6b3
unknown <nonni1234567@gmail.com>
2019-10-23 22:31:55 +0000

    kláraði allt og lét líta vel út

--- app.py
+++ app.py
@@ -11,10 +11,13 @@ with open("static/quiz.json", "r", encoding="UTF-8") as f:
 
 @app.route("/") # Root Routeið
 def home():
-    return template("index.html")
+    if "svor" in session: # Resettar cookies í hvert sinn sem þú ferð á root
+        session.pop("svor")
+        session.pop("teljari")
+    return template("index.html",quizzes=quizzes)
 
 @app.route("/quiz/<name>")
-def quiz(name):
+def quiz(name): # Gefur quiz eftir vali
     teljari = 0 # Redefinear teljara
     svor = {}
     if "teljari" in session: # Ef teljari er í session þá er hann notaður
@@ -22,18 +25,18 @@ def quiz(name):
     else: # Ef ekki þá er hann settur inn í session
         session["teljari"] = teljari
     
-    if "svor" in session:
+    if "svor" in session: # Sama og með teljara
         svor = session["svor"]
     else:
         session["svor"] = svor
 
     return template("quiz.html",spurning = quizzes[name][teljari], quiznafn=name)
 
-
 @app.route("/next/<name>", methods=["post"]) # Næsta spurning (hækkar teljara um eitt)
 def next(name):
     svar = request.form["svar"]
     svor = {}
+    rett = 0
     teljari = 0
     if "svor" in session:
         svor = session["svor"]
@@ -41,30 +44,37 @@ def next(name):
         session["svor"] = svor
     if "teljari" in session: # Notar teljara inn í session ef hann er til
         teljari = session["teljari"]
-
         svor[quizzes[name][teljari][1]] = [quizzes[name][teljari][2], svar] # setur spurningu, rétta svarið og giskaða svarið í dictionary með session
+        if "rett" in session: # Notar rétt teljaran í session ef hægt
+            rett = session["rett"]
         if "svor" in session:
             session["svor"] = svor
         else:
             svor = session["svor"]
-        if teljari >= len(quizzes["shrek"])-1:
-            # ef spurningarnar eru búnar fer þetta í results sem verður einhvernveginn
-            # þetta er það eina sem ég er búin að gera ég kann ekki owo
-            return redirect("/results")
+        svor[quizzes[name][teljari][1]] = [quizzes[name][teljari][2], svar] # setur spurningu, rétta svarið og giskaða svarið í dictionary með sessions
+        templisti = svor[quizzes[name][teljari][1]]
+        if templisti[0].lower() == templisti[1].lower():
+                rett += 1 # Hækkar rétt teljara um eitt ef svarið er rétt
+        if teljari >= len(quizzes[name])-1:
+            # ef spurningarnar eru búnar fer þetta í results
+            session["rett"] = rett
+            return redirect(f"/results/{name}")
         else:
             teljari += 1
         session["teljari"] = teljari
+        session["rett"] = rett
         
     else: # Ef það er enginn teljari inn í Session þá gerist ekkert
-        pass
+        return redirect("/")
     
     return redirect(f"/quiz/{name}")
 
 
-@app.route("/results")
-def result(): # Result fyrir spurningarnar
-    if "svor" in session:
+@app.route("/results/<name>")
+def result(name): # Result fyrir spurningarnar
+    if "svor" in session and "rett" in session:
         svor = session["svor"]
+        rett = session["rett"]
     else:
         # engin svör voru sett í sessionið svo hér hefur eitthvað farið úrskeiðis
         # kannski /results skrifað beint inn í browser
@@ -73,10 +83,8 @@ def result(): # Result fyrir spurningarnar
 
     session.pop("svor")
     session.pop("teljari")
-
-    return template("result.html",svor=svor)
-
-
+    session.pop("rett")
+    return template("result.html", svor=svor, rett=rett, total = len(quizzes[name]))
 
 @app.errorhandler(404)
 def pagenotfound(error):
